4e6e60be268c93c4a71b2aac2a14440fb07ef18f
azharprabudi <azharprabui@gmail.com>
2019-12-27 17:33:55 +0700

    add common styling

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,8 @@
 import * as React from "react";
 import * as ReactDOM from "react-dom";
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
-import 'bulma/css/bulma.css'
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom"
+import "bulma/css/bulma.css"
+import "./styles/common.styl"
 import BackendDrawer from "./pages/backend-drawer";
 
 const Main: React.SFC<{}> = () => (
