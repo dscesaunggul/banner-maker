098db10544e986043ea9a471cadad70d5f21fbe9
azharprabudi <azharprabui@gmail.com>
2019-12-27 18:54:30 +0700

    apply prettier

--- src/index.tsx
+++ src/index.tsx
@@ -1,15 +1,15 @@
 import * as React from "react";
 import * as ReactDOM from "react-dom";
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom"
-import "bulma/css/bulma.css"
-import "./styles/common.styl"
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import "bulma/css/bulma.css";
+import "./styles/common.styl";
 import BackendDrawer from "./pages/backend-drawer";
 
 const Main: React.SFC<{}> = () => (
   <Router>
     <Switch>
-      <Route exact path="/" >
-          <BackendDrawer/>
+      <Route exact path="/">
+        <BackendDrawer />
       </Route>
     </Switch>
   </Router>
