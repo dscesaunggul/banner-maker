8877309edae0f779dae8df6e9d18fd553422e918
azharprabudi <azharprabui@gmail.com>
2019-12-28 14:20:37 +0700

    change context schema for supporting local reducer

--- src/pages/backend-drawer/context/index.tsx
+++ src/pages/backend-drawer/context/index.tsx
@@ -1,5 +1,29 @@
 import * as React from "react";
 
-export function StateContext<T>(defaultValue: T): React.Context<T> {
-  return React.createContext<T>(defaultValue);
-}
+type Background = {
+  filename: string;
+  base64: string;
+};
+
+type Canvas = {
+  background: Background;
+  objects: object[];
+};
+
+export type State = {
+  canvas: Canvas;
+  editable: false;
+};
+
+export const defaultValue: State = {
+  canvas: {
+    background: {
+      filename: "",
+      base64: ""
+    },
+    objects: []
+  },
+  editable: false
+};
+
+export const { Provider, Consumer } = React.createContext<State>(defaultValue);
